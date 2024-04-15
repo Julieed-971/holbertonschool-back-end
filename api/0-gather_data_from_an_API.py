f460bbc89927aaf80f3609116804f04555cba4cd
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-15 15:33:07 +0200

    Fixed pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,20 +8,21 @@ from sys import argv
 if __name__ == "__main__":
 
     # Get user name related to id
-    user_api_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    user_api_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        argv[1])
     user = requests.get(user_api_url)
     employee_name = user.json().get('name')
 
     # Get tasks from user
-    tasks_api_url = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(
+    tasks_url = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(
         argv[1])
-    todos = requests.get(tasks_api_url)
+    todos = requests.get(tasks_url)
     todos_tasks = todos.json()
     total_tasks = len(todos_tasks)
 
     # Get and append task title for done task in todos_tasks
     titles = [item.get('title') for item in todos_tasks if
-            item.get("completed") is True]
+              item.get("completed") is True]
     done_tasks = len(titles)
 
     print("Employee {} is done with tasks({}/{}):".format(
