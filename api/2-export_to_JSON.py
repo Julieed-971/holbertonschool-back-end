585bd511e91690893ac1dac758c54636d8a88878
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-16 09:58:18 +0200

    Export to JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Export data from employees information to CSV file"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    # Get user name related to id
+    user_api_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        argv[1])
+    user = requests.get(user_api_url)
+    employee_name = user.json().get('username')
+    id = user.json().get('id')
+
+    # Get tasks from user
+    tasks_url = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(
+        argv[1])
+    todos = requests.get(tasks_url)
+    todos_tasks = todos.json()
+
+    # Create the list of dict with values to export to json
+    todos_list = []
+    for item in todos_tasks:
+        todos_list.append({
+            "task": item["title"],
+            "completed": item["completed"],
+            "username": employee_name
+        })
+
+    # Export data to a csv file
+    json_file = "{}.json".format(argv[1])
+    with open(json_file, "w") as f:
+        json.dump([argv[1], todos_list], f)
